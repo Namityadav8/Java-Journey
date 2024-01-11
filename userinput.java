3a1a37622d731bbd0a86cddef2c7b76dec5129f2
Namit <namityadav008@gmail.com>
2024-01-11 22:30:11 +0530

    changes

--- userinput.java
+++ userinput.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class userinput {
     public static void main(String[] args) {
-        System.out.println("user input process");
+        // System.out.println("user input process");
         Scanner a = new Scanner(System.in);
-        System.out.println("Enter number 1");
-        int b = a.nextInt();
-        System.out.println("number 2");
-        int d = a.nextInt();
-       
+        // System.out.println("Enter number 1");
+        // int b = a.nextInt();
+        // System.out.println("number 2");
+        // int d = a.nextInt();
+       String str = a.nextLine ();
+       System.out.println(str);
     }
 }
