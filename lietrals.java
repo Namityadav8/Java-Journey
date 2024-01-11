66ff3f6451f568d0bc345071dc20571926c56e72
Namit <namityadav008@gmail.com>
2024-01-11 21:55:12 +0530

    changes

---
+++ lietrals.java
@@ -0,0 +1,7 @@
+public class lietrals {
+    public static void main(String[] args) {
+        int age = 128;
+        String a = "harr";
+        System.out.println(age);
+    }
+}
---
+++ userinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[] args) {
+        System.out.println("user input process");
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int b = a.nextInt();
+        System.out.println("number 2");
+        int d = a.nextInt();
+       
+    }
+}
