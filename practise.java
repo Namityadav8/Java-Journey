1d450616ba8419ed785b4860e26edeb5f0a2b72a
Namit <namityadav008@gmail.com>
2024-01-12 20:59:26 +0530

    changes

--- practise.java
+++ practise.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class practise {
     public static void main(String[] args) {
-        
+        int a;
+        Scanner b = new Scanner(System.in);
+        int c = b.nextInt();
+        System.out.println(c);
+
     }
 }
