a73b14864412bc93c9f63322789e345e00bea99d
Namit <namityadav008@gmail.com>
2024-01-13 15:05:55 +0530

    strings

--- Expressions.java
+++ Expressions.java
@@ -2,10 +2,16 @@
 public class Expressions {
 
     public static void main(String[] args) {
-        int a=3;
-        System.out.println(a++);
-        System.out.println(++a);
-        System.out.println(a--);
-        System.out.println(--a);
+        // int a=3;
+        // System.out.println(a++);
+        // System.out.println(++a);
+        // System.out.println(a--);
+        // System.out.println(--a);
+        // char a = 'A';
+        // int b = 5;
+        float c = 5.647568342652f;
+        // System.out.printf("value of b and c is %d and %f " , b , c);
+        System.out.printf("value of c is %.4f",c);
+        // System.out.println(++a + ++b); 
     }
 }
