a88d6e69d5fa83d91ad34d421d6af42ca7e6fbe9
Namit <namityadav008@gmail.com>
2024-01-15 02:56:33 +0530

    changes in code and added some more programs

--- loops.java
+++ loops.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
        
-        int b = 7;
-        while(b<=10){
-            System.out.println("Namit is horny as fuck ");
-            b++;
-        }
+    int b = 10;
+    for(int i=5;i<=10;i++){
+        System.out.println("namit is horny as fuck ");
     }
-}
+       }
+    }
+
