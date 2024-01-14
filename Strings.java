a410ab0f539a482879f2833d9673fc21f76b337c
Namit <namityadav008@gmail.com>
2024-01-14 11:11:43 +0530

    changes

---
+++ Strings.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        String a = "Namit";
+    //     Scanner  a = new Scanner(System.in);
+
+    //     String b = a.nextLine();
+    //     System.out.println(b);
+    // }
+    // String a = "Namit";
+    // int value = a.length();
+    // System.out.println(value);
+
+    // String b = a.toUpperCase();
+    // System.out.println(b);
+
+    // String b = a.trim();
+    // System.out.println(b);
+    // System.out.println(a.substring(3,5));
+    // // System.out.println(a.replace("l","pussy is f"));
+    // System.out.println(a.startsWith("Na"));
+
+    // System.out.println(a.charAt(3 ));
+
+    System.out.println(a.indexOf("Namit"));
+
+
+    }
+}
