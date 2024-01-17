7a2ad2546270cc67b756332d8ea9eca4f939c807
Namit <namityadav008@gmail.com>
2024-01-17 23:50:37 +0530

    changes

--- Dynamic_Input.java
+++ Dynamic_Input.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Dynamic_Input {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        int m ;
-        System.out.print("Enter the number you want to take in array \n");
-        m  = a.nextInt();
-        int [] b = new int[m];
-        for(int i=0;i<m;i++){
-            b[i] = a.nextInt();
-        }
+       int m;
+       System.out.println("Enter how many numbers you want in your array :");
+       m=a.nextInt();
+       int [] b = new int[m];
+       for(int i=0;i<m;i++){
+        b[i] = a.nextInt();
+       }
+
        for(int i=0;i<m;i++){
         System.out.println(b[i]);
        }
+
+
 
     }
 }
