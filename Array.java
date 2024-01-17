a2f796741740332ba84389a214508c2abe1944cb
Namit <namityadav008@gmail.com>
2024-01-17 21:53:49 +0530

    changes in code

--- Array.java
+++ Array.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         /*how to initialise an array */
-        Scanner b = new Scanner(System.in);
-        int [] a = new int[5];
-        for(int i =0;i<=5;i++){
-            a[i] = Scanner.nextInt();
+        //there are three ways to create arrays in java 
+        // int [] a = new int[5];
+        // System.out.println(a[3]);
+        // int [] a ;
+        // a= new int[10];
+        // a[0] = 2;
+        // System.out.println(a[0]);
+        // String [] students;
+        // students = new[10];
+        Scanner a = new Scanner(System.in);
+
+        int [] b = new int[10];
+        for(int i=0;i<5;i++){
+            b[i] = a.nextInt();
         }
-        System.out.println(a);
+
+        for(int i=0;i<5;i++){
+            System.out.println(b[i]);
+        }
+
     }
 }
