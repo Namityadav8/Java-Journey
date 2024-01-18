07738f370a662751f71dcbd5f67914e360fedf5b
Namit <namityadav008@gmail.com>
2024-01-18 23:31:21 +0530

    changes

--- D2_Array.java
+++ D2_Array.java
@@ -4,12 +4,13 @@ public class D2_Array {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         int m,n;
-        System.out.println("Enter the number of rows and columns you want to add in your array :");
+        System.out.println("No. of Rows and Columns you want : ");
+
         m=a.nextInt();
         n=a.nextInt();
 
         int [][] c = new int[m][n];
-
+        System.out.println("Enter your values of array ");
         for(int i=0;i<c.length;i++){  ///here c.length is used as in 2D array arrayname.length gives us the output of the numbrs of the rows in the array
             for(int j=0;j<c[i].length;j++){
                 c[i][j] = a.nextInt();
@@ -19,7 +20,7 @@ public class D2_Array {
 
         //now after taking all the inputs in the 2d array now we have to print all the arrays 
 
-
+        System.out.println("The array you have made is : ");
         for(int i=0;i<c.length;i++){
             for(int j=0;j<c[i].length;j++){
                 System.out.print(c[i][j]);
