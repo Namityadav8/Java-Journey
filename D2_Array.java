7810a135e3a7b1dfeb89322ab349cdd0d1ba5af7
Namit <namityadav008@gmail.com>
2024-01-18 23:20:05 +0530

    changes in code

--- D2_Array.java
+++ D2_Array.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
+
 public class D2_Array {
     public static void main(String[] args) {
-        Scanner b = new Scanner(System.in);
-        //declaration of an 2-D array
-       int [][] a = new int[2][3];
+        Scanner a = new Scanner(System.in);
+        int m,n;
+        System.out.println("Enter the number of rows and columns you want to add in your array :");
+        m=a.nextInt();
+        n=a.nextInt();
 
+        int [][] c = new int[m][n];
 
-       for(int i=0;i<a.length;i++){
-        for(int j=0;j<a[i].length;j++){
-            System.out.print(a[i][j]);
-            System.out.print(" ");
+        for(int i=0;i<c.length;i++){  ///here c.length is used as in 2D array arrayname.length gives us the output of the numbrs of the rows in the array
+            for(int j=0;j<c[i].length;j++){
+                c[i][j] = a.nextInt();
+            }
+            System.out.println("");
         }
-        System.out.println("");
-       }
+
+        //now after taking all the inputs in the 2d array now we have to print all the arrays 
+
+
+        for(int i=0;i<c.length;i++){
+            for(int j=0;j<c[i].length;j++){
+                System.out.print(c[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        
     }
+    
 }
-
-
--- practise.java
+++ practise.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class practise {
     public static void main(String[] args) {
-        Scanner b = new Scanner(System.in);
-        int [] a ;
-        a = new int[5];
-        for(int i=0;i<5;i++){
-            a[i] = b.nextInt();
-        }
+        Scanner a = new Scanner(System.in);
+        int [][] b ;
+        b = new int[3][5];
 
-        for(int i=0;i<5;i++){
-            System.out.println(a[i]);
+        for(int i=0;i<b.length;i++){
+            for(int j=0;j<b[i].length;j++){
+                System.out.print(b[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
         }
-        for(int bitch : a){
-            System.out.println(bitch);
-        }
-    
+       
+        
         
     }
     
