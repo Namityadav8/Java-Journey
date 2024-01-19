c841e9e6cf72e9b5321f31b41376ae6588b08b54
Namit <namityadav008@gmail.com>
2024-01-19 13:31:43 +0530

    changes in an array 2D

--- practise.java
+++ practise.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-
 public class practise {
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        int [][] b ;
-        b = new int[3][5];
+        Scanner b = new Scanner(System.in);
+        int [][] a = new int[2][3];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                a[i][j] = b.nextInt();
+            }
+             System.out.println("");
+        }
 
-        for(int i=0;i<b.length;i++){
-            for(int j=0;j<b[i].length;j++){
-                System.out.print(b[i][j]);
+        System.out.println("these numbers you have chosen in array");
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                System.out.print(a[i][j]);
                 System.out.print(" ");
             }
-            System.out.println("");
+             System.out.println("");
         }
-       
-        
-        
-    }
-    
+    }   
 }
