91847bde1c1c11650fb64066b83cf6bcdafe23af
Namit <namityadav008@gmail.com>
2024-01-20 18:22:05 +0530

    changes in code

---
+++ Recursion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Recursion {
+
+    static int factorial(int n){
+            if(n==0 || n==1){
+                return 1;
+            }else if(n<0){
+                return 0;
+            }else{
+                return n*factorial(n-1); // like this here we have called the function inside the 
+                // function so it will be called again by itself .
+            }
+
+        }
+    
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter the number you want factorial of : ");
+        int b = a.nextInt();
+        System.out.println(factorial(b));
+    }
+}
--- Variable_arguments.java
+++ Variable_arguments.java
@@ -2,14 +2,15 @@
  * Variable_arguments
  */
 public class Variable_arguments {
-    static int vararg(int ...a){
-        int result = 0 ;
+    static int vararg(int x,int ...a){
+        int result = x ;
         for(int b : a){ // here a will take inputs of all the elements as a array but it isnt an actual array.
             result += b;
         }
-        return result;
+       return result;
     }
     public static void main(String[] args) {
+        System.out.println(vararg(1)); 
         System.out.println(vararg(2,3,4,5));
         System.out.println(vararg(3,4,5,5,6));
         
