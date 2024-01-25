307d6deaae3fa2e394a83de27a589cd47a984493
Namit <namityadav008@gmail.com>
2024-01-25 23:09:48 +0530

    abstract methods and classes

--- abstract_method.java
+++ abstract_method.java
@@ -1,9 +1,33 @@
-class base{
-    public void 
+abstract class base{
+    public void x(){
+        System.out.println("Base class me hu");
+    }
+
+    abstract public void greet();// here the asbtract method is created
+    abstract public void sayhello();
+}
+
+abstract class derived extends base{
+    public void nk(){
+        System.out.println("khana kha lo gays");
+    }
+}
+
+class derived2 extends base{
+    public void greet(){
+        System.out.println("namaste");
+    }
+
+    public void sayhello(){
+        System.out.println("hello");
+    }
 }
 public class abstract_method {
     public static void main(String[] args) {
-        
+        derived2 obj = new derived2();
+        //base obj1 = new base(); // obj of abstract class can't created 
+        //object of concrete class is used to access all the methods in asbtract and concrete class
+        obj.greet();
         
     }
 }
