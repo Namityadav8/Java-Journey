58f186010a32eeb0b18c13e08be9ed9c9801a758
Namit <namityadav008@gmail.com>
2024-01-26 16:48:56 +0530

    added abstract classes and interfaces

--- interfaces.java
+++ interfaces.java
@@ -1,4 +1,5 @@
 interface bicycle{
+    public final int b =23;
     void brake(int decreament);
     void speed(int increament);
 }
@@ -27,7 +28,7 @@ public class interfaces {
     public static void main(String[] args) {
         Avon bike = new Avon();
         bike.brake(12);
-
+        System.out.println(bike.b); // since it is a int value we have to store it in a other integer or print it 
         bike.RamMandir();
     }
 }
