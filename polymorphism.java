9fd2259af879e80794badbd13671d5ad2171a1b9
Namit <namityadav008@gmail.com>
2024-01-27 02:22:27 +0530

    polymorphism is added in it .

--- polymorphism.java
+++ polymorphism.java
@@ -1,5 +1,23 @@
+interface car{
+    void company(); //one interface is having diff. forms in diff class
+}                   // each func can only access its defined methods and func in interface
+
+class toyota implements car{
+    public void company(){
+        System.out.println("fartuner vali hai");
+    }
+}
+
+class tesla implements car{
+    public void company(){
+        System.out.println("elon musk vali ");
+    }
+}
 public class polymorphism {
     public static void main(String[] args) {
-        
+        car b = new toyota();
+        car c = new tesla();
+        b.company();   // interface cant make objects here it is only reffering to interface 
+        c.company();
     }
 }
