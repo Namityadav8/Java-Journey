319dc6ce3aae068eb93f8a8d1f6b41db3c43da58
Namit <namityadav008@gmail.com>
2024-01-28 17:10:37 +0530

    changes in code

--- Array.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Array {
-    public static void main(String[] args) {
-       
-
-
-
-    }
-}
--- packages.java
+++ packages.java
@@ -1,7 +1,7 @@
 // import java.util.Scanner;
 // import java.util.*; // will import all the libraries 
 
-package namit;
+package namit.yadav;   // this is package inside a package which means yadav folder inside namit
 
 public class packages {
     public static void main(String[] args) {
