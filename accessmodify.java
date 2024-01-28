830235e97ad0f469409f984f5b9ae446565c88d5
Namit <namityadav008@gmail.com>
2024-01-28 18:28:00 +0530

    access modifiers in java

---
+++ accessmodify.java
@@ -0,0 +1,12 @@
+class A{
+    public int a = 23;
+    public int b = 256;
+    protected int c = 231;
+    int d = 90;
+}
+public class accessmodify {
+    public static void main(String[] args) {
+        A x = new A();
+        
+    }
+}
