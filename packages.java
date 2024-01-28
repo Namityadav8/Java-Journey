ce246f889d6ca9a846977ec3b20db17f6349e26a
Namit <namityadav008@gmail.com>
2024-01-28 16:24:03 +0530

    learned about packages

---
+++ packages.java
@@ -0,0 +1,12 @@
+// import java.util.Scanner;
+// import java.util.*; // will import all the libraries 
+
+package namit;
+
+public class packages {
+    public static void main(String[] args) {
+        java.util.Scanner a = new java.util.Scanner(System.in); // without importing
+        int b = a.nextInt();
+        System.out.println("this is my entered number from scanner " + b);
+    }
+}
