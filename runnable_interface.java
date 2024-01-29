4d74f9117f15b89258d1b40a5dc5ae1b271e142b
Namit <namityadav008@gmail.com>
2024-01-29 19:04:34 +0530

    runnable interface added

---
+++ runnable_interface.java
@@ -0,0 +1,24 @@
+class A implements Runnable{
+    public void run(){
+        System.out.println("inside thread 1.");
+    }
+}
+
+class B implements Runnable{
+    public void run(){
+        System.out.println("Inside thread 2.");
+    }
+}
+public class runnable_interface {
+
+    public static void main(String[] args) {
+        A obj = new A(); 
+        B obj1 = new B();
+
+        Thread ram = new Thread(obj);
+        Thread sita = new Thread(obj1);
+
+        ram.start();
+        sita.start();
+    }
+}
