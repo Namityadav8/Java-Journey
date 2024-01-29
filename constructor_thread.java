079986895d3f9a1186991f3b7ff6609d9c0471fa
Namit <namityadav008@gmail.com>
2024-01-29 21:57:05 +0530

    added some more methods

--- constructor_thread.java
+++ constructor_thread.java
@@ -1,20 +1,14 @@
-class thr extends Thread{
-    public thr(String name){
-        super(name); // here we are passing that name to the const of superclass.
-    }
+class namit implements Runnable{
     public void run(){
-        System.out.println("ram ram");
+        System.out.println("hi i am in thread");
     }
 }
+
 public class constructor_thread {
     public static void main(String[] args) {
-        thr a = new thr("namit");
-        thr b = new thr("harsh");
+        namit a = new namit();
 
-        System.out.println("ID of namit is " + a.getId());
-        System.out.println("name is " + a.getName());
-
-        System.out.println("id of harsh is " + b.getId());
-        System.out.println("name of harsh is " + b.getName());
+        Thread obj = new Thread(a , "My thread is ");
+        obj.start();
     }
 }
