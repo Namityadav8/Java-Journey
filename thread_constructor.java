dd4eedcd152639582810c3d1829ddee4ee085fb3
Namit <namityadav008@gmail.com>
2024-02-05 01:25:57 +0530

    priorities in threads

--- thread_constructor.java
+++ thread_constructor.java
@@ -4,6 +4,7 @@ class Mythr extends Thread{
     }
     public void run(){
         System.out.println("hi");
+        
     }
 
 }
@@ -13,12 +14,12 @@ public class thread_constructor {
     public static void main(String[] args) {
         Mythr a = new Mythr("namit");
         Mythr b = new Mythr("yadav");
-
+        a.setPriority(Thread.MAX_PRIORITY);
         a.start();
         b.start();
         System.out.println("The id of thread is "+ a.getId());
         System.out.println("the name of thread is "+ a.getName());
 
-        
+
     }
 }
