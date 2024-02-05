44f69d937fb43e9bd9fb3660117439d4fa000e79
Namit <namityadav008@gmail.com>
2024-02-05 21:09:19 +0530

    changes in code

--- practise1.java
+++ practise1.java
@@ -4,13 +4,13 @@ class namit extends Thread{
     }
 
     public namit(String name){
-        super(name);
+        super(name); // since it is derived class this name is passed to base class 
     }
 }
 public class practise1 {
     public static void main(String[] args) {
-        namit yadav = new namit("kesar");
-        Thread t = new Thread();
-      t.start();
+        namit yadav = new namit("kesar"); // passed to thread
+        yadav.start();
+        System.out.println(yadav.getName()); //
     }
 }
