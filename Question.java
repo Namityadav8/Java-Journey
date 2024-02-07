1f228620e0cd567199e71e4b51aff5a59388c1b0
Namit <namityadav008@gmail.com>
2024-02-07 10:38:54 +0530

    changes in code

---
+++ Question.java
@@ -0,0 +1,13 @@
+public class Question {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,5};
+        for(int i:a){
+            if(i%2==0){
+                System.out.println(i +" is even");
+            }
+            else{
+                System.out.println(i+" is odd");
+            }
+        }
+    }
+}
---
+++ java_class.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class java_class {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+
+        // String b = a.nextLine();
+        // char c = a.next().charAt(0);
+        // int d = a.nextInt();
+        // float e = a.nextFloat();
+        int b [] = {1,23,4555,7,8};
+
+    }
+}
