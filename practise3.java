149253717e20ef57496ad42c3befa5d541117d14
Namit <namityadav008@gmail.com>
2024-02-16 22:37:43 +0530

    changes in my code as per my needs

---
+++ practise3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class practise3 {
+
+    public static void main(String[] args) {
+      String a = "Namit";
+      char b =a.charAt(3);
+      System.out.println(b);
+    }
+}
