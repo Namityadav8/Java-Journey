9ec6dd62443b5d03ff6f1198d23a8babc58bce49
Namit <namityadav008@gmail.com>
2024-02-19 18:01:50 +0530

    learned a new thing

--- practise3.java
+++ practise3.java
@@ -1,29 +1,16 @@
-import java.util.*;
+/**
+ * practise3
+ */
+import java.util.Scanner;
 public class practise3 {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-   
-    //     int [][] a = new int[2][2];
-    //     for(int i=0;i<a.length;i++){
-    //         for(int j=0;j<a[i].length;j++){
-    //             a[i][j] = sc.nextInt();
-    //         }
-    //         System.out.println("");
-    // }
+        Scanner a = new Scanner(System.in);
+        int x =a.nextInt();
 
-   
-    //     for(int i=0;i<a.length;i++){
-    //         for(int j=0;j<a[i].length;j++){
-    //             System.out.print(a[i][j]);
-    //             System.out.print(" ");
-    //         }
-    //         System.out.println("");
-    //     }
-
-
-
-
-    
+        while(x>=10){
+            x=x/10;
+        }
+        System.out.println(x);
     }
 }
