9b2de189d5ac943a511fb599797eaa9143fa5bce
Namit <namityadav008@gmail.com>
2024-02-22 14:14:39 +0530

    practised some questions

--- practise3.java
+++ practise3.java
@@ -1,16 +1,21 @@
-/**
- * practise3
- */
-import java.util.Scanner;
+import java.util.*;
 public class practise3 {
+    public static void fib(int n){
+        int a = 0;
+        int b =1;
 
+        System.out.print(a+" "+b+" ");
+
+        for(int i=2;i<n;i++){
+            int next = a+b;
+            System.out.print(next+" ");
+            a=b;
+            b=next;
+        }
+    }
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        int x =a.nextInt();
-
-        while(x>=10){
-            x=x/10;
-        }
-        System.out.println(x);
+        int y = a.nextInt();
+        fib(y);
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,2 +1,19 @@
- String x = "Namit";
-        System.out.println(x.substring(3));
+public class practise3 {
+    public static void main(String[] args) {
+        int n = 10; // Number of Fibonacci terms to generate
+        System.out.println("Fibonacci Series:");
+        printFibonacci(n);
+    }
+
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print(a + " " + b + " ");
+        
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(next + " ");
+            a = b;
+            b = next;
+        }
+    }
+}
