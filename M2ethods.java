3b67ddec70d40dcff16ff657694a25e077280b3e
Namit <namityadav008@gmail.com>
2024-03-13 09:48:52 +0530

    abc

--- M2ethods.java
+++ M2ethods.java
@@ -4,7 +4,7 @@
 public class M2ethods {
 
      void Sum(){ // in this case it wont be returning any value as it is void function .
-        System.out.println("gand marao");
+        System.out.println("hello");
     }
     public static void main(String[] args) {
         M2ethods m = new M2ethods();
