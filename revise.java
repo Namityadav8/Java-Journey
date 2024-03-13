e2b8538a78ae44714765d9aa2e9f898a93f59d4b
Namit <namityadav008@gmail.com>
2024-03-13 22:29:48 +0530

    made some changes in the code

---
+++ revise.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class revise {
+    public int sum(int a , int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        // System.out.println("Enter the number of elements you want to add in your matrix :");
+        // int m = x.nextInt();
+        // int n =x.nextInt();
+        // System.out.println("Now print the value of the matrix ");
+        // int [][] a = new int[m][n];
+        // for(int i=0;i<a.length;i++){
+        //     for(int j=0;j<a[i].length;j++){
+        //         a[i][j] = x.nextInt();
+        //     }
+        // }
+        // System.out.println("Now printing the matrix that you have created :");
+        // for(int i=0;i<a.length;i++){
+        //     for(int j=0;j<a[i].length;j++){
+        //         System.out.print(a[i][j]);
+        //         System.out.print(" ");
+        //     }
+        //     System.out.println("");
+        // }
+        revise obj = new revise();
+        // System.out.println(obj.sum(23,345));
+        
+
+
+
+
+
+    }
+}
