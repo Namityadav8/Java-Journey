d6c65dddde7f5ac1a9aaac66dcb91455fc1b8e49
Namit <namityadav008@gmail.com>
2024-03-19 22:34:11 +0530

    changes made

--- java_class.java
+++ java_class.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
 
 public class java_class {
-    public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        String s = a.nextLine();
 
-        char[] c = s.toCharArray();
-        int m = c.length - 1;
-        for (int i = m; i >= 0; i--) {
-            System.out.print(c[i]);
-        }
+    public static void main(String[] args) {
+        
     }
 }
