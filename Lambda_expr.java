77294030debb789419dd5b13ee7262c229cea779
Namit <namityadav008@gmail.com>
2024-03-19 21:40:44 +0530

    made some changes

--- Lambda_expr.java
+++ Lambda_expr.java
@@ -1,12 +1,23 @@
 @FunctionalInterface
 interface a{
-    void fun();
+    void fun(int a );
 }
 
 
 public class Lambda_expr {
     public static void main(String[] args) {
-        a obj = () -> System.out.println("hello world");
-        obj.fun();
+        a obj = (int x ) -> {
+            int a = 0, b = 1;
+            System.out.println("First " + x + " Fibonacci numbers:");
+            System.out.print(a + " " + b + " "); 
+    
+            for (int i = 2; i < x; i++) {
+                int c = a + b; 
+                System.out.print(c + " ");
+                a = b; 
+                b = c; 
+            }
+        };
+        obj.fun(6);
     }
-}
+} 
