6aefd1a12114fdb798a704d72334dab79ff74a61
Namit <namityadav008@gmail.com>
2024-03-21 22:07:18 +0530

    learning

--- java_class.java
+++ java_class.java
@@ -1,25 +1,20 @@
 class A{
-    public A(int a ){
-        System.out.println("value of a is "+a);
-    }
-    public A(){
-        System.out.println("Necessary to have default const as there is a default in extended one ");
+    public void hello(){
+        System.out.println("hello");
     }
 }
 
 class B extends A{
-    public B(){
-        super(34);
-        System.out.println("Inside class B");
+    public void hello(){
+        super.hello();
+        System.out.println("hello bahisaab");
     }
 
-    public B(int b){
-        System.out.println("The value of b is "+b);
-    }
 }
 public class java_class {
 
     public static void main(String[] args) {
-        B b = new B(323);
+        B b = new B();
+        b.hello();
     }
 }
