4a784d1a84609e257d22fd32517aac0104649414
Namit <namityadav008@gmail.com>
2024-03-21 21:44:04 +0530

    revising all concepts of java

--- java_class.java
+++ java_class.java
@@ -1,7 +1,25 @@
+class A{
+    public A(int a ){
+        System.out.println("value of a is "+a);
+    }
+    public A(){
+        System.out.println("Necessary to have default const as there is a default in extended one ");
+    }
+}
 
+class B extends A{
+    public B(){
+        super(34);
+        System.out.println("Inside class B");
+    }
+
+    public B(int b){
+        System.out.println("The value of b is "+b);
+    }
+}
 public class java_class {
 
     public static void main(String[] args) {
-        
+        B b = new B(323);
     }
 }
