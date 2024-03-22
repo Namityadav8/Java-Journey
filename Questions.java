8df16b9f977cdc43099c2523e0e02fc112e85c58
Namit <namityadav008@gmail.com>
2024-03-22 10:45:36 +0530

    practised date and time in java

---
+++ Questions.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.Month;
+public class Questions {
+    public static void main(String[] args) {
+        //Q.1.
+        LocalDate a = LocalDate.of(2007,Month.JANUARY,3).plusDays(90);
+        System.out.println(a);
+
+
+        //Q.2.
+        LocalDate d = LocalDate.of(2007,Month.JANUARY,3).plusMonths(11);
+        System.out.println(d);
+
+
+        //Q.3.
+        
+    }
+}
