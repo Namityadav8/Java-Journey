7111760009fe1ec1ec137bbf6261586e7f3e7b74
Namit <namityadav008@gmail.com>
2024-03-29 22:02:02 +0530

    threads

--- revise.java
+++ revise.java
@@ -1,20 +1,20 @@
-class a implements Runnable{
+class a extends Thread{
     public void run(){
         for(int i=0;i<10;i++){
             System.out.println(i);
             try {
                 Thread.sleep(100);
             } catch (Exception e) {
-                // TODO: handle exception
+                System.out.println("Error");
             }
         }
+
     }
 }
 public class revise {
 
     public static void main(String[] args) {
-        a obj = new a();
-        Thread sc = new Thread(obj);
-        sc.start();
+        a aa = new a();
+        aa.start();
     }
 }
