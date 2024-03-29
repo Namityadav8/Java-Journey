f06757b3c68c0cc9bd268ac49b578559c49893c3
Namit <namityadav008@gmail.com>
2024-03-30 01:58:29 +0530

    made some changes

--- revise.java
+++ revise.java
@@ -1,14 +1,25 @@
-interface A{
-    public void hello();
+interface a{
+    public void meth1();
+}
+
+interface b{
+    public void meth2();
 }
 public class revise {
     public static void main(String[] args) {
-        A obj = () ->
-            {
-                System.out.println("ha bhai ");
-            };
-        
+        a obj = new a(){
+            public void meth1(){
+                System.out.println("It is Method 1");
+            }
+        };
+        obj.meth1();
+        b obj1 = new b(){
+            public void meth2(){
+                System.out.println("It is Method 2");
+            }
+        };
+        obj1.meth2();
 
-        obj.hello();
     }
+    
 }
