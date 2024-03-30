d1960bc16fd25f86fc7252a202376a12cbb47cec
Namit <namityadav008@gmail.com>
2024-03-30 10:44:56 +0530

    amde some changes

--- Date_Time_.java
+++ Date_Time_.java
@@ -1,5 +1,16 @@
+import java.util.Date;
+import java.time.*;
 public class Date_Time_ {
     public static void main(String[] args) {
         
+        
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
     }
 }
