6ba594d388f822b35d65086d40dd8b542c6afa20
Namit <namityadav008@gmail.com>
2024-04-05 09:49:08 +0530

    made some changes

--- FileHandling.java
+++ FileHandling.java
@@ -1,19 +1,78 @@
+// import java.io.File;
+// import java.io.IOException;
+// public class FileHandling {
+//     public static void main(String[] args) {
+//     //     try{
+//     //         File fo = new File("input.txt");
+//     //         if(fo.createNewFile()){
+//     //             System.out.println("File "+fo.getName()+" is created successflly.");
+//     //         }else{
+//     //             System.out.println("File is already exist in the directry.");
+//     //         }
+        
+//     // }catch(IOException exception){
+//     //     System.out.println("An unexpected error is occured.");
+//     //     exception.printStackTrace();
+//     //     }
+
+//     // System.out.println();
+       
+        
+//     }
+// }
+
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.*;
 public class FileHandling {
     public static void main(String[] args) {
-        try{
-            File fo = new File("input.txt");
-            if(fo.createNewFile()){
-                System.out.println("File "+fo.getName()+" is created successflly.");
-            }else{
-                System.out.println("File is already exist in the directry.");
-            }
+        // String basePath = "C:\\Local volume\\Programming\\Java\\IO\\";
+        // String fileName = "input.txt";
+        // File obj = new File(basePath + fileName);
+
+        // try {
+        //     if (obj.exists()) {
+        //         int count = 1;
+        //         do {
+        //             fileName = "input" + count + ".txt";
+        //             fileName = "input"   + count + ".txt";
+        //             obj = new File(basePath + fileName);
+        //             count++;
+        //         } while (obj.exists());
+        //     }
+
+        //     if (obj.createNewFile()) {
+        //         System.out.println("File " + obj.getName() + " is created successfully");
+        //     } else {
+        //         System.out.println("File already exists");
+        //     }
+        // } catch (IOException exception) {
+        //     System.out.println("An unexpected error");
+        //     exception.printStackTrace();
+        // }
+
+        // System.out.println("Path: " + obj.getAbsolutePath());
+        // System.out.println("Writable: " + obj.canWrite());
+        // System.out.println("Readable: " + obj.canRead());
+        // System.out.println("Length in bytes: " + obj.length());
+        // System.out.println("Length in bytes: " + obj.length());
         
-    }catch(IOException exception){
-        System.out.println("An unexpected error is occured.");
-        exception.printStackTrace();
+
+
+        try{
+            File f1 = new File("input.txt");
+            Scanner dataReader = new Scanner(f1);
+            while(dataReader.hasNextLine()){
+                String fileData = dataReader.nextLine();
+                System.out.println(fileData);
+            }
+            dataReader.close();
+        }catch(FileNotFoundException exception){
+            System.out.println("Unexpected error occured");
+            exception.printStackTrace();
         }
+        
     }
 }
-
