5be5785db3132813adc38999acb4096d27720f40
Namit <namityadav008@gmail.com>
2024-04-17 09:29:43 +0530

    made some new changes

--- exception.java
+++ exception.java
@@ -1,19 +1,14 @@
+/**
+ * exception
+ */
 public class exception {
     public static void main(String[] args) {
         try {
-            // Code that may throw exceptions
-            int[] array = new int[5];
-            array[4] = 1/0; // This will throw an ArrayIndexOutOfBoundsException
-        } catch (ArithmeticException e) {
-            // Handle arithmetic exceptions
-            System.err.println("An arithmetic exception occurred: " + e.getMessage());
-        } catch (ArrayIndexOutOfBoundsException e) {
-            // Handle array index out of bounds exceptions
-            System.err.println("An array index out of bounds exception occurred: " + e.getMessage());
+            int a[]=new int[5];
+            a[6]=3;
+            System.out.println(a[6]);
         } catch (Exception e) {
-            // Handle any other exceptions
-            System.err.println("An unexpected exception occurred: " + e.getMessage());
+            System.out.println(e);
         }
-        
     }
 }
