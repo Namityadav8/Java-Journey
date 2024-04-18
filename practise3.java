00c84d809a539e720b4a8207ef6c5eceb6f38740
Namit <namityadav008@gmail.com>
2024-04-19 01:42:16 +0530

    learning concepts of exception Handling

--- practise3.java
+++ practise3.java
@@ -1,19 +1,21 @@
+import java.util.Scanner;
+
 /**
  * practise3
  */
 public class practise3 {
-    public static String getMessage(){
-            return "it is a defined error";
-    }
+    
     public static void main(String[] args) {
-        int a =1/0;
-       
-        try {
-            System.out.println(a);
-        } catch (Exception e) {
-            System.out.println(e.getMessage() );
-          
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        if(a==3){
+            throw new ArithmeticException();
         }
+        else{
+            System.out.println("Alright");
+        }
+       
 
     }
 }
