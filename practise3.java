0986c92363064f922b0c5cf7bbf64a2af7590a8e
Namit <namityadav008@gmail.com>
2024-04-19 01:02:48 +0530

    made some changes in R

--- practise3.java
+++ practise3.java
@@ -2,14 +2,18 @@
  * practise3
  */
 public class practise3 {
-
+    public static String getMessage(){
+            return "it is a defined error";
+    }
     public static void main(String[] args) {
         int a =1/0;
+       
         try {
             System.out.println(a);
         } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
-            e.printStackTrace();
+            System.out.println(e.getMessage() );
+          
         }
+
     }
 }
