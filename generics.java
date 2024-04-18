6a5b933678f52fe0ba7d79dd8815d6974b3bb3f9
Namit <namityadav008@gmail.com>
2024-04-18 20:52:33 +0530

    made some changes in javav

--- generics.java
+++ generics.java
@@ -1,16 +1,40 @@
-import java.util.*;
-class A<T,U>{
-    T a;
-    void fun(T b , U c){
-        a=b;
-        System.out.print(b+" "+c);
-    }
+// import java.util.*;
+// class A<T,U>{
+//     T a;
+//     void fun(T b , U c){
+//         int a1 = (Integer)b*(Integer)b;
+//         System.out.print(a1);
+//     }
+// }
+// public class generics {
+//     public static void main(String[] args) {
+//         A<Integer,String> obj = new A<Integer,String>();
+//         A<String,Integer> obj1 = new A<String,Integer>();
+//         obj.fun(23,"james");
+//         obj1.fun("Namit",8);
+//     }
+// }
+
+
+
+//calculating average of an elements in an array
+
+import java.lang.reflect.Array;
+
+class A<t>{
+   t[] a;
+   void avg(t a){
+        for(int i=0;i<a.;i++){
+
+        }
+   }
+    
+
 }
 public class generics {
+
     public static void main(String[] args) {
-        A<Integer,String> obj = new A<Integer,String>();
-        A<String,Integer> obj1 = new A<String,Integer>();
-        obj.fun(23,"james");
-        obj1.fun("Namit",8);
+        A<Array> obj = new A<Array>();
+
     }
 }
