047f5aff2586686f10d312693c1bbc013577455d
Namit <namityadav008@gmail.com>
2024-04-18 20:54:39 +0530

    exception handling in java

--- practise3.java
+++ practise3.java
@@ -4,6 +4,11 @@
 public class practise3 {
 
     public static void main(String[] args) {
-        
+        int a =1/0;
+        try {
+            System.out.println(a);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }
