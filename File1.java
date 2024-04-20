fb850cecf6cbb7668278212db9c94dca8a61d482
Namit <namityadav008@gmail.com>
2024-04-20 18:37:36 +0530

    made some changes

--- File1.java
+++ File1.java
@@ -1,18 +1,28 @@
+import java.io.*;
 import java.io.File;
-/**
- * File1
- */
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 public class File1 {
-
     public static void main(String[] args) {
-        File a = new File("namit.txt");
-        try {
-            a.createNewFile();
-        } catch (Exception e) {
-           System.out.println(e);
+        // code wo create a new file 
+        File f1 = new File("C:\\Academics\\Java-Journey\\namit.txt");
+        f1.exists();  // to check if a file is existing or not 
+        try{
+            f1.createNewFile();
+        }catch(IOException e){
+            System.out.println(e);
         }
-        finally{
-            System.out.println("File created without any error");
+        /// code to write in a file \
+        try{
+        FileWriter fq = new FileWriter(f1);
+        fq.write("hi this is namit yadav");
+        fq.close();
+        }catch(Exception e ){
+            System.out.println(e);
         }
+
+
+        //reading a file 
     }
 }
