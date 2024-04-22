8f1358d2c8bc766a6c25bae92ad41a2856dbe884
Namit <namityadav008@gmail.com>
2024-04-22 22:12:34 +0530

    made some changes

---
+++ collections.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class collections {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        a.add(3);
+        a.add(34);
+        b.add(345);
+        b.add(21);
+        a.addAll(b);
+        a.add(0,12); // adding any values to any particular index 
+        System.out.println(a.contains(35));
+     
+        System.out.println(a);
+        System.out.println(a.get(3));
+        
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,17 +1,1 @@
-try{
-            File obj=new File("nlp.txt");
-            Scanner sc=new Scanner(obj);
-            String larger="";
-            String current;
-            while (sc.hasNext()) {
-            current=sc.next();
-            if (current.length()>larger.length()){
-            larger=current;
-            }
-            }
-            System.out.println(larger);
-            
-            }
-            catch(Exception e){
-                System.out.println(e);
-            }
+b
