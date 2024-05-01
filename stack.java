061950f24f0bbf00e4515cde5800244a6c3bcb3a
Namit <namityadav008@gmail.com>
2024-05-01 10:11:25 +0530

    added stack

---
+++ stack.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> obj = new Stack<>();
+        obj.add(23);
+        obj.add(20);
+        obj.add(21);
+        obj.add(22);
+        obj.add(25);
+        System.out.println(obj);
+
+    }
+}
