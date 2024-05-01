3fbe153305c4f21c7df303d09047b9a612cde28f
Namit <namityadav008@gmail.com>
2024-05-01 10:26:09 +0530

    functions on Stack

--- stack.java
+++ stack.java
@@ -7,7 +7,19 @@ public class stack {
         obj.add(21);
         obj.add(22);
         obj.add(25);
-        System.out.println(obj);
-
+       System.out.println(obj);
+       obj.push(69);
+       System.out.println(obj);
+        while(!obj.isEmpty()){
+            System.out.println(obj.pop());
+        }
+    
+      
     }
 }
+
+// obj.pop()   returns the topmost value of the stack 
+// push adds items into it 
+// pop returns the top most element
+//esEmpty() checks if the stack is empty or not 
+// peek() returns the first element in the stack 
