41f954a459e86d309bab7d217a27e6a3900c92ba
Namit <namityadav008@gmail.com>
2024-05-09 00:49:09 +0530

    ooo

--- ETP.java
+++ ETP.java
@@ -1,12 +1,42 @@
 import java.util.*;
+import java.io.*;
 public class ETP {
     public static void main(String[] args) {
+       
         
-        String ok = "adityauis23879649287basjkdb";
+        File ok = new File("opg.txt");
 
-        String ok2 = "askjfvb";
-        String a = ok.concat(ok2);
-        System.out.println(a);
+        try{
+
+            ok.createNewFile();
+
+            FileWriter lo = new FileWriter(ok);
+
+            lo.write("12\n");
+            lo.write("11\n");
+            lo.write("13\n");
+            lo.close();
+
+            Scanner sc = new Scanner(ok);
+            int k = 0;
+            int s = 0;
+            while(sc.hasNextLine()){
+            String c = sc.nextLine();
+            Integer q = new Integer(c);
+            k+=q;
+            s++;
+            }
+
+            System.out.println(k/s);
+
+
+            
+
+        }
+        catch(Exception e){
+            System.out.println("inininininininin");
+        }
+
 
 
 
