42210fd9980bcedd76d06df8c8c299ce445d0a4b
Namit <namityadav008@gmail.com>
2024-05-09 00:22:35 +0530

    ETP

---
+++ ETP.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ETP {
+    public static void main(String[] args) {
+        
+        String ok = "adityauis23879649287basjkdb";
+
+        String ok2 = "askjfvb";
+        String a = ok.concat(ok2);
+        System.out.println(a);
+
+
+
+
+    }
+}
