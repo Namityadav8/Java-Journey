3cb9f3098e5b975fcb491eef1af7e9adf0aaba47
Namit <namityadav008@gmail.com>
2024-05-09 22:14:41 +0530

    made some chanegs

--- CodingCompetitionEligibility.java
+++ CodingCompetitionEligibility.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
-
+       
 public class CodingCompetitionEligibility {
+
+
+    public static int sum(int ...a){
+        int result = 0 ;
+        for(int i: a){
+            result = result +i;
+        }
+            return result; 
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String a = "my name is namit";
-        StringBuilder sb = new StringBuilder();
-        sb.append(a);
+        System.out.println(sum(23,34,23,21,23,34,1));
+        System.out.println(sum(23,34,23,34,1));
        
-        sb.insert(3,"amit ");
-        System.out.println(sb);
-
+        
     }
 }
