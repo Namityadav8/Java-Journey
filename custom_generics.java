5178fde8386dae383034247066f9215d611ea22e
Namit <namityadav008@gmail.com>
2024-05-13 17:08:43 +0530

    made some changes

--- custom_generics.java
+++ custom_generics.java
@@ -1,4 +1,9 @@
-
+class a<t>{
+    int a = 3 ;
+    public static <t> sum(t a , t b){
+        return t a - t b;
+    }
+}
 
 public class custom_generics {
     public static void main(String[] args) {
