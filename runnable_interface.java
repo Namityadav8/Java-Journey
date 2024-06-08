9ac082aae877cae7d54c71c8f677329c058cdc38
Namit <namityadav008@gmail.com>
2024-06-09 02:10:10 +0530

    revised the concepts of java

--- runnable_interface.java
+++ runnable_interface.java
@@ -1,24 +1,26 @@
 class A implements Runnable{
     public void run(){
+        for(int i=0;i<10;i++){
         System.out.println("inside thread 1.");
+        try{
+            Thread.sleep(100);
+        }catch(Exception e){
+            
+        }
+        }
     }
 }
 
-class B implements Runnable{
-    public void run(){
-        System.out.println("Inside thread 2.");
-    }
-}
+
 public class runnable_interface {
     
     public static void main(String[] args) {
         A obj = new A(); 
-        B obj1 = new B();
+        
 
         Thread ram = new Thread(obj);
-        Thread sita = new Thread(obj1);
+        
 
         ram.start();
-        sita.start();
-    }
+    }   
 }
