2e7d953807110030b071b801068602a6db72906b
Namit <namityadav008@gmail.com>
2024-08-15 21:59:08 +0530

    basics

--- Complete_Revise.java
+++ Complete_Revise.java
@@ -4,8 +4,9 @@ public class Complete_Revise {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    
-    
+    double d = 3.4;
+    char a = sc.next().charAt(0);
+    System.out.println(a);
 
   }
 }
