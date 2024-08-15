5278205c497c20abc53fda0396ec59c00d0b6731
Namit <namityadav008@gmail.com>
2024-08-15 21:46:52 +0530

    made some changes

--- Complete_Revise.java
+++ Complete_Revise.java
@@ -1,12 +1,11 @@
+import java.util.Scanner;
 
 public class Complete_Revise {
 
-    public static void main(String[] args){
-        
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    
 
-
-
-
-
-    }
+  }
 }
