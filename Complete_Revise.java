7c6319eeb8c2b9d712f892ed4222820736b7f90e
Namit <namityadav008@gmail.com>
2024-08-18 18:00:00 +0530

    made some changes

--- Complete_Revise.java
+++ Complete_Revise.java
@@ -1,26 +1,28 @@
-class a{
-    public a(){
-        System.out.println("inside parent class ");
-    }
-    public a(int a){
-        System.out.println("Inside parent class paramterised"+a);
-    }
-}
-class b extends a{
-    public b(){
-        System.out.println("inside child class ");
-    }
-    public b(int b){
-        super(23);
-        System.out.println("Inside child class paramterised"+b);
-    }
-}
+import java.io.*;
+
+
 public class Complete_Revise {
-        public static void main(String[] args) {
-        Complete_Revise cr = new Complete_Revise();
-        a aa = new a();
-        b bb = new b(23);
-       
+
+    public static void main(String[] args) {
+        File f1 = new File("moose.txt");
+        // try{
+        //     f1.createNewFile();
+        // }catch(Exception e ){
+        //     System.out.println("File isnt created due to this reason "+e);
+        // }finally{
+        //     System.out.println("File is been created .");
+        // }
+
+        System.out.println("Now writing inside the file ");
         
+        try{    
+            FileWriter fw = new FileWriter(f1);
+            fw.write("Hello this is been written in the file ");
+            fw.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }finally{
+            System.out.println("Content has been written in the file ");
+        }
     }
 }
